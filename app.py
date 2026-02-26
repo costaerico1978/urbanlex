e4c810dedec66e4aa292b4a4b4dcf45cea70bbb2
costaerico1978 <145128064+costaerico1978@users.noreply.github.com>
2026-02-25 23:16:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1440,4 +1440,4 @@ def inicializar():
 inicializar()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT',5000)), debug=os.ge
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT',5000)), debug=os.getenv('FLASK_ENV')!='production')
